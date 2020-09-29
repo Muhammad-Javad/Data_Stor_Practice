c2662c612251a014371b91379925eb870eb419e9
Muhammad-Javad <44828583+Muhammad-Javad@users.noreply.github.com>
2020-09-29 20:08:49 +0330

    add PreferenceActivity.kt's logic

--- app/src/main/java/com/javadsh98/datastorepractice/PreferenceActivity.kt
+++ app/src/main/java/com/javadsh98/datastorepractice/PreferenceActivity.kt
@@ -1,13 +1,70 @@
 package com.javadsh98.datastorepractice
 
+import android.graphics.ColorFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.activity_preference.*
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class PreferenceActivity : AppCompatActivity() {
+
+    val settingManager by lazy {
+        SettingManager(this)
+    }
+    val dark = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preference)
 
-
+        setTheme()
+        observeNight()
     }
+
+    private fun setTheme() {
+        imageView.setOnClickListener {
+            lifecycleScope.launch {
+                when(dark){
+                    true -> {
+                        settingManager.setTheme(ThemeMode.DARK)
+                    }
+
+                    false -> {
+                        settingManager.setTheme(ThemeMode.LIGHT)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun observeNight() {
+
+        lifecycleScope.launch {
+            settingManager.getTheme.collect {
+                when(it){
+                    ThemeMode.LIGHT -> {
+                        setLight()
+                    }
+                    ThemeMode.DARK -> {
+                        setNight()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setLight() {
+        constraintlayout_preference.setBackgroundColor(ContextCompat.getColor(this, android.R.color.white))
+        imageView.setImageResource(R.drawable.ic_baseline_wb_sunny_24)
+    }
+
+    private fun setNight() {
+        constraintlayout_preference.setBackgroundColor(ContextCompat.getColor(this, android.R.color.darker_gray))
+        imageView.setImageResource(R.drawable.moon)
+    }
+
+
 }
