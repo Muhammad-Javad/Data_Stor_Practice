36c3f3323dc2cbd5b6f52a62b45685859b1e3c18
Muhammad-Javad <44828583+Muhammad-Javad@users.noreply.github.com>
2020-09-29 17:25:18 +0330

    add PreferenceActivity.kt

---
+++ app/src/main/java/com/javadsh98/datastorepractice/PreferenceActivity.kt
@@ -0,0 +1,11 @@
+package com.javadsh98.datastorepractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preference)
+    }
+}
