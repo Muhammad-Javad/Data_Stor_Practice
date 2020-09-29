b149a77424e1c343971d2c2fd194f49a249a03b8
Muhammad-Javad <44828583+Muhammad-Javad@users.noreply.github.com>
2020-09-29 17:32:42 +0330

    add PreferenceKey

--- app/src/main/java/com/javadsh98/datastorepractice/SettingManager.kt
+++ app/src/main/java/com/javadsh98/datastorepractice/SettingManager.kt
@@ -2,9 +2,15 @@ package com.javadsh98.datastorepractice
 
 import android.content.Context
 import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.preferencesKey
 
 class SettingManager(context: Context){
 
     val dataStore = context.createDataStore("javad_setting")
 
+
+
+    companion object{
+        val NIGHT_KEY = preferencesKey<Boolean>("dark_mode")
+    }
 }
