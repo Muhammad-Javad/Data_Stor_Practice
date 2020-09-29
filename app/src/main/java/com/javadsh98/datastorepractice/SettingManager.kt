b30f99d564688e9e41b2326d9984345844a9c804
Muhammad-Javad <44828583+Muhammad-Javad@users.noreply.github.com>
2020-09-29 17:38:39 +0330

    add a method for save nightmode value

--- app/src/main/java/com/javadsh98/datastorepractice/SettingManager.kt
+++ app/src/main/java/com/javadsh98/datastorepractice/SettingManager.kt
@@ -1,16 +1,30 @@
 package com.javadsh98.datastorepractice
 
 import android.content.Context
+import android.graphics.LightingColorFilter
 import androidx.datastore.preferences.createDataStore
+import androidx.datastore.preferences.edit
 import androidx.datastore.preferences.preferencesKey
 
-class SettingManager(context: Context){
+enum class ThemeMode {
+    LIGHT, DARK
+}
+
+class SettingManager(context: Context) {
 
     val dataStore = context.createDataStore("javad_setting")
 
+    suspend fun setTheme(themeMode: ThemeMode) {
+        dataStore.edit { preference ->
+            preference[NIGHT_KEY] = when (themeMode) {
+                ThemeMode.DARK -> true
+                ThemeMode.LIGHT -> false
+            }
 
+        }
+    }
 
-    companion object{
+    companion object {
         val NIGHT_KEY = preferencesKey<Boolean>("dark_mode")
     }
 }
