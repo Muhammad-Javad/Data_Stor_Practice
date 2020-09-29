b84bce0d62b3938a184d5c140829e4e693b932dd
Muhammad-Javad <44828583+Muhammad-Javad@users.noreply.github.com>
2020-09-29 17:56:24 +0330

    add a field for get our data from datastor

--- app/src/main/java/com/javadsh98/datastorepractice/SettingManager.kt
+++ app/src/main/java/com/javadsh98/datastorepractice/SettingManager.kt
@@ -4,7 +4,12 @@ import android.content.Context
 import android.graphics.LightingColorFilter
 import androidx.datastore.preferences.createDataStore
 import androidx.datastore.preferences.edit
+import androidx.datastore.preferences.emptyPreferences
 import androidx.datastore.preferences.preferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
 
 enum class ThemeMode {
     LIGHT, DARK
@@ -13,6 +18,23 @@ enum class ThemeMode {
 class SettingManager(context: Context) {
 
     val dataStore = context.createDataStore("javad_setting")
+
+    val getTheme: Flow<ThemeMode> = dataStore.data
+        .catch {
+            if (it is IOException){
+                it.printStackTrace()
+                emit(emptyPreferences())
+            }else{
+                throw it
+            }
+        }
+        .map {
+            val pref = it[NIGHT_KEY] ?: false
+            when(pref){
+                true -> ThemeMode.DARK
+                false -> ThemeMode.LIGHT
+            }
+        }
 
     suspend fun setTheme(themeMode: ThemeMode) {
         dataStore.edit { preference ->
