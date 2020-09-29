a5feb4e3e95b4aabbdf9ef4e53a9f852d9f7ab68
Muhammad-Javad <44828583+Muhammad-Javad@users.noreply.github.com>
2020-09-29 18:03:19 +0330

    setup PreferenceActivity.kt layout

--- app/src/main/java/com/javadsh98/datastorepractice/PreferenceActivity.kt
+++ app/src/main/java/com/javadsh98/datastorepractice/PreferenceActivity.kt
@@ -7,5 +7,7 @@ class PreferenceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preference)
+
+
     }
 }
